6959a95aaf62a40ec582a715e3b025708e3b9a2f
Irasov <srnan@yandex.ru>
2025-07-03 12:36:55 +0500

    feat: add video block

--- scripts/sript.js
+++ scripts/sript.js
@@ -1,6 +1,8 @@
 const icon = document.querySelector(".header__icon");
 const nav = document.querySelector(".header__nav");
 const menu = document.querySelector(".header__menu");
+const player = document.querySelector(".video__player");
+const control = document.querySelector(".video__control");
 if(icon) {
     icon.addEventListener('click', () => {
         document.body.classList.toggle('_lock');
@@ -8,4 +10,14 @@ if(icon) {
         nav.classList.toggle('_active');
         icon.classList.toggle('_active');
     }); 
+}
+
+if(player) {
+    if(control) {
+        control.addEventListener('click', () => {
+            player.setAttribute('controls', '');
+            player.play();
+            control.style.display = 'none';
+        })
+    }
 }
